6b53a6ee47f785caeef520064613ae6d8ed74f24
Alexander58123 <alexandr.luckjanov@yandex.ru>
2023-07-12 14:34:43 +0300

    nachalo 7block

---
+++ app/src/main/java/com/example/zadanie6/Profile.kt
@@ -0,0 +1,21 @@
+package com.example.zadanie6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+      //  val bottomNavigationView: BottomNavigationView = findViewById(R.id.BottomNavagation);
+      //  bottomNavigationView.itemIconTintList = null
+    }
+
+
+
+
+}
