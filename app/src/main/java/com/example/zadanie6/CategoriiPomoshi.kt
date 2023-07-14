52f4d9f9e7087427a3c3958356b1e2664a1e9134
Alexander58123 <alexandr.luckjanov@yandex.ru>
2023-07-14 11:07:36 +0300

    camera and delete photo

---
+++ app/src/main/java/com/example/zadanie6/CategoriiPomoshi.kt
@@ -0,0 +1,11 @@
+package com.example.zadanie6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CategoriiPomoshi : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_categorii_pomoshi)
+    }
+}
--- app/src/main/java/com/example/zadanie6/Profile.kt
+++ app/src/main/java/com/example/zadanie6/Profile.kt
@@ -1,36 +1,82 @@
 package com.example.zadanie6
 
+import android.Manifest
 import android.app.Dialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Profile : AppCompatActivity() {
 
     // это чтобы доступ был
     lateinit var imageProfile : ImageView
+    lateinit var imageProfileOriginal : Bitmap
     lateinit var dialog : Dialog
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
-      //  val bottomNavigationView: BottomNavigationView = findViewById(R.id.BottomNavagation);
-      //  bottomNavigationView.itemIconTintList = null
-
-        imageProfile = findViewById(R.id.man_profile)
+        imageProfile = findViewById(R.id.imageView3)
         dialog = Dialog(this)
+
+        // сохраняем изначальную картинку
+        var drawable = imageProfile.drawable as BitmapDrawable
+        imageProfileOriginal = drawable.bitmap
+
+
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.CAMERA), 100
+            )
+        }
     }
 
+    // открытие окна диалога по картинке профиля
     fun showDialog(view : View) {
         dialog.setContentView(R.layout.dialog_profile)
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.WHITE))
         dialog.show()
     }
 
+    // открытие камеры
+    fun showCamera(view : View) {
+        var intent: Intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 100)
+        dialog.hide()
+    }
+
+    // удаление фотографии (возвращаю первоначальную)
+    fun deleteFotoProfile(view : View) {
+        // imageProfile.setImageDrawable(null)
+        imageProfile.setImageBitmap(imageProfileOriginal)
+        dialog.hide()
+    }
+
+    // замена изображения в профиле
+    override fun onActivityResult(requestCode : Int, resultCode : Int, data : Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == 100) {
+            val captureImage = data?.extras?.get("data") as Bitmap
+            imageProfile.setImageBitmap(captureImage)
+        }
+    }
 
 }
