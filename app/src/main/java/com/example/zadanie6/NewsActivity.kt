f5651cfd08c32b9ef7a3ff1d6937d1052e9115db
Alexander58123 <alexandr.luckjanov@yandex.ru>
2023-08-12 14:58:44 +0300

    read and output RecyclerView in NewsActivity

--- app/src/main/java/com/example/zadanie6/NewsActivity.kt
+++ app/src/main/java/com/example/zadanie6/NewsActivity.kt
@@ -4,7 +4,6 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
-import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +11,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.InputStream
+import java.lang.Exception
 
 class NewsActivity : AppCompatActivity() {
 
@@ -23,10 +26,6 @@ class NewsActivity : AppCompatActivity() {
     lateinit var sharedPreferences2: SharedPreferences
     lateinit var sharedPreferences3: SharedPreferences
     lateinit var sharedPreferences4: SharedPreferences
-    lateinit var boolean1: TextView
-    lateinit var boolean2: TextView
-    lateinit var boolean3: TextView
-    lateinit var boolean4: TextView
 
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: NewsAdapter
@@ -41,18 +40,13 @@ class NewsActivity : AppCompatActivity() {
         nav = findViewById(R.id.BottomNavagation)
         buttonFilter = findViewById(R.id.filterButton)
 
-//        boolean1 = findViewById(R.id.boolean1)
-//        boolean2 = findViewById(R.id.boolean2)
-//        boolean3 = findViewById(R.id.boolean3)
-//        boolean4 = findViewById(R.id.boolean4)
-
         recyclerView = findViewById(R.id.RecyclerNovosti)
         recyclerView.layoutManager = LinearLayoutManager(this)
 
         spisokForAdapter = initSpisokNewsData()
 
         adapter = NewsAdapter(initSpisokNewsData())
-        recyclerView.adapter = adapter  // передаем в адаптер наш список с данными
+        recyclerView.adapter = adapter // передаем в адаптер наш список с данными
 
 //        sharedPreferences1 = this.getSharedPreferences("hranilishe1", MODE_PRIVATE)
 //        sharedPreferences2 = this.getSharedPreferences("hranilishe2", MODE_PRIVATE)
@@ -62,8 +56,6 @@ class NewsActivity : AppCompatActivity() {
 //        boolean2.text = sharedPreferences2.getBoolean("value2", true).toString()
 //        boolean3.text = sharedPreferences3.getBoolean("value3", true).toString()
 //        boolean4.text = sharedPreferences4.getBoolean("value4", true).toString()
-
-
 
         // открытие меню Помочь
         buttonhead.setOnClickListener {
@@ -100,15 +92,46 @@ class NewsActivity : AppCompatActivity() {
         }
     }
 
-    fun initSpisokNewsData() : List<NewsData> {
-
+    // инициализация списка для адаптера в RecyclerView
+    fun initSpisokNewsData(): List<NewsData> {
         var spisok = mutableListOf<NewsData>()
-            spisok.add(NewsData(R.drawable.news1, "Заголовок1", "Описание события тут много текста и прочее", "здесь дата1"))
-            spisok.add(NewsData(R.drawable.news2, "Заголовок2", "Описание события тут много текста и прочее", "здесь дата2"))
+
+        try {
+            var jsonObject: JSONObject = JSONObject(JsonDataFromAsset("sobutie.json")) // строку принимает
+            var jsonArray: JSONArray = jsonObject.getJSONArray("sobutie")
+            for (i in 0 until jsonArray.length()) {
+                var sobutieData: JSONObject = jsonArray.getJSONObject(i)
+                spisok.add(
+                    NewsData(
+                        resources.getIdentifier(sobutieData.getString("kartinka"), "drawable", packageName),
+                        sobutieData.getString("title"),
+                        sobutieData.getString("description"),
+                        sobutieData.getString("data"),
+                    ),
+                )
+            }
+        } catch (ex: Exception) {
+            ex.stackTrace
+        }
 
         return spisok
     }
 
+    // метод считываем весь файл json (байты), возвращаем String
+    fun JsonDataFromAsset(filename: String): String? {
+        var json: String? = null
+        try {
+            var inputStream: InputStream = assets.open(filename)
+            var sizeOfFile: Int = inputStream.available()
+            var bufferData = ByteArray(sizeOfFile)
+            inputStream.read(bufferData)
+            inputStream.close()
+            json = String(bufferData, Charsets.UTF_8)
+        } catch (ex: Exception) {
+            ex.stackTrace
+        }
+        return json
+    }
 
 //    override fun onResume() {
 //        super.onResume()
