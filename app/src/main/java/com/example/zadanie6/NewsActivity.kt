a3368b1b28e75d4be0793454cbb612eb2bc0173d
Alexander58123 <alexandr.luckjanov@yandex.ru>
2023-08-30 14:11:50 +0300

    add savedInstanceState NewsActivity

--- app/src/main/java/com/example/zadanie6/NewsActivity.kt
+++ app/src/main/java/com/example/zadanie6/NewsActivity.kt
@@ -3,6 +3,7 @@ package com.example.zadanie6
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Parcelable
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
@@ -15,6 +16,7 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.InputStream
+import java.io.Serializable
 import java.lang.Exception
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
@@ -30,6 +32,7 @@ class NewsActivity : AppCompatActivity() {
     lateinit var recyclerView: RecyclerView
     lateinit var adapter: NewsAdapter
     lateinit var spisokForAdapter: List<NewsData>
+    lateinit var mListState: Parcelable
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,40 +51,63 @@ class NewsActivity : AppCompatActivity() {
         spisokForAdapter = emptyList()
         adapter = NewsAdapter()
 
-        val service: ExecutorService = Executors.newSingleThreadExecutor()
-        service.execute(
-            Runnable {
-                runOnUiThread(
-                    Runnable {
-                        progressBar.visibility = View.VISIBLE
-                    },
-                )
+        // здесь восстанавливаем состояние, если изменилась ориентация
+        if (savedInstanceState != null) {
+            progressBar.visibility = View.GONE
+            val spisokFromSave = savedInstanceState.getSerializable("key_state") as List<NewsData>
+            spisokForAdapter = spisokFromSave
+            adapter.setData(spisokForAdapter)
+            recyclerView.adapter = adapter // передаем в адаптер наш список с данными
+            adapter.notifyDataSetChanged()
 
-                Thread.sleep(5000)
+            // если пустой список, показываем уведомление
+            if (spisokFromSave.isEmpty()) {
+                emptyText.visibility = View.VISIBLE
+            }
 
-                runOnUiThread(
-                    Runnable {
-                        spisokForAdapter = initSpisokNewsData()
-                        adapter.setData(spisokForAdapter)
-                        recyclerView.adapter = adapter // передаем в адаптер наш список с данными
-                        adapter.notifyDataSetChanged()
-                        progressBar.visibility = View.GONE
-                    },
-                )
+            //  слушатель для адаптера через анонимный класс
+            adapter.setOnClickListener(object : NewsAdapter.OnClickListener {
+                override fun onClick(position: Int, model: NewsData) {
+                    val tekushiiObject = spisokForAdapter[position]
+                    val intent = Intent(this@NewsActivity, SobutiePodrobno::class.java)
+                    intent.putExtra("sobutieData", tekushiiObject)
+                    startActivity(intent)
+                }
+            })
+        } else { // первый запуск без смены ориентации
+            val service: ExecutorService = Executors.newSingleThreadExecutor()
+            service.execute(
+                Runnable {
+                    runOnUiThread(
+                        Runnable {
+                            progressBar.visibility = View.VISIBLE
+                        },
+                    )
 
-                //  слушатель для адаптера через анонимный класс
-                adapter.setOnClickListener(object : NewsAdapter.OnClickListener {
-                    override fun onClick(position: Int, model: NewsData) {
-                        val tekushiiObject = spisokForAdapter[position]
-                        val intent = Intent(this@NewsActivity, SobutiePodrobno::class.java)
-                        intent.putExtra("sobutieData", tekushiiObject)
-                        startActivity(intent)
-                    }
-                })
-            },
-        )
+                    Thread.sleep(5000)
 
+                    runOnUiThread(
+                        Runnable {
+                            spisokForAdapter = initSpisokNewsData()
+                            adapter.setData(spisokForAdapter)
+                            recyclerView.adapter = adapter // передаем в адаптер наш список с данными
+                            adapter.notifyDataSetChanged()
+                            progressBar.visibility = View.GONE
+                        },
+                    )
 
+                    //  слушатель для адаптера через анонимный класс
+                    adapter.setOnClickListener(object : NewsAdapter.OnClickListener {
+                        override fun onClick(position: Int, model: NewsData) {
+                            val tekushiiObject = spisokForAdapter[position]
+                            val intent = Intent(this@NewsActivity, SobutiePodrobno::class.java)
+                            intent.putExtra("sobutieData", tekushiiObject)
+                            startActivity(intent)
+                        }
+                    })
+                },
+            )
+        }
 
         // открытие меню Помочь
         buttonhead.setOnClickListener {
@@ -110,8 +136,12 @@ class NewsActivity : AppCompatActivity() {
 
             return@setOnItemSelectedListener true
         }
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
 
+        outState.putSerializable("key_state", spisokForAdapter as Serializable)
     }
 
     // инициализация списка для адаптера в RecyclerView
@@ -184,8 +214,6 @@ class NewsActivity : AppCompatActivity() {
                         }
                     }
                 }
-
-
             }
             // если пустой список, показываем уведомление
             if (spisok.isEmpty()) {
